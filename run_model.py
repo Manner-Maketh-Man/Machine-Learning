edf4a764ffcba2871e45f7df086be9d75c0a3107
DabinLee1881 <98525706+DabinLee1881@users.noreply.github.com>
2023-05-23 21:53:02 +0900

    fix kobert model loading

--- run_model.py
+++ run_model.py
@@ -3,16 +3,85 @@ import pandas as pd
 import re
 
 import torch
-import torch.nn as nn
+import torch as nn
 import torch.nn.functional as F
-import gluonnlp as nlp
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
-from transformers import BertModel, BertTokenizer
 from transformers import logging
-import multiprocessing
 logging.set_verbosity_error() # ingnore transformer warning
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+import gluonnlp as nlp
 
+# kobert model
+bertmodel, vocab = get_pytorch_kobert_model()
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+PATH_TO_MODEL_FILE = "KoBERT_ver1_state_dict.pt"
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=7,
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+class Kobert:
+    def __init__(self, device):
+        self.device = device
+        self.bertmodel, self.vocab = get_pytorch_kobert_model()
+        self.tokenizer = nlp.data.BERTSPTokenizer(get_tokenizer(), vocab, lower=False)  # use the created tokenizer
+        self.model = BERTClassifier(bertmodel, dr_rate=0.5).to(device) # BERTClassifier instance
+        self.model.load_state_dict(torch.load(PATH_TO_MODEL_FILE, map_location=device))
+        self.model.eval()
+
+    def predict(self, sent):
+        # Tokenize the input sentence
+        tokens = self.tokenizer(sent)
+
+        # Convert tokens to token IDs
+        token_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+
+        # Convert the token IDs to a tensor
+        token_tensor = torch.tensor([token_ids])
+
+        # Move the tensor to the appropriate device
+        token_tensor = token_tensor.to(self.device)
+
+        # Create a tensor for segment ids
+        segment_tensor = torch.zeros_like(token_tensor)
+        
+        # Forward pass through the model
+        with torch.no_grad():
+            outputs = self.model(token_tensor, [len(token_ids)], segment_tensor)
+
+        # Get the predicted class index
+        predicted_index = torch.argmax(outputs)
+
+        return predicted_index.item()
+    
 # kogpt model
 class Kogpt:
     # check model path
